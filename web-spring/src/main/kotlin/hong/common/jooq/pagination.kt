aa03170eafa24a037ee7f3e64b366560fa2a2b40
이용홍 <hong@mine-k.co.kr>
2020-01-09 20:18:52 +0900

    Pagination 추가

---
+++ web-spring/src/main/kotlin/hong/common/jooq/pagination.kt
@@ -0,0 +1,63 @@
+package hong.common.jooq
+
+import org.jooq.DSLContext
+import org.jooq.Record
+import org.jooq.SelectLimitStep
+import kotlin.math.ceil
+import kotlin.math.min
+
+private fun intCeil(x: Int, y: Int): Int = ceil(x.toDouble() / y).toInt()
+
+class Pagination<E>(
+    val page: Int,
+    val pageSize: Int,
+    val total: Long,
+    val content: List<E>,
+    val totalPages: Int,
+    val pages: List<Int>,
+    val hasPrevious: Boolean,
+    val hasNext: Boolean,
+    val isFirst: Boolean,
+    val isLast: Boolean,
+    val hasContent: Boolean
+) {
+
+    companion object {
+        fun <R : Record, E> of(
+            ctx: DSLContext,
+            query: SelectLimitStep<R>,
+            mapper: (record: R) -> E,
+            page: Int,
+            pageSize: Int = 20,
+            navSize: Int = 10
+        ): Pagination<E> {
+            val total = ctx.fetchCount(query)
+            val offset = (page - 1) * pageSize
+            val content = query.limit(offset, pageSize).map(mapper)
+
+            val totalPages = if (pageSize == 0) 1 else ceil(total.toDouble() / pageSize.toDouble()).toInt()
+            val hasPrevious = page > 1
+            val hasNext = page < totalPages
+            val isFirst = !hasPrevious
+            val isLast = !hasNext
+            val hasContent = content.isNotEmpty()
+
+            val navHead = navSize * (ceil(page.toDouble() / navSize.toDouble()).toInt() - 1) + 1
+            val navTail = min(totalPages, navHead + navSize - 1)
+
+            return Pagination(
+                page,
+                pageSize,
+                total.toLong(),
+                content,
+                totalPages,
+                (navHead..navTail).toList(),
+                hasPrevious,
+                hasNext,
+                isFirst,
+                isLast,
+                hasContent
+            )
+        }
+    }
+}
