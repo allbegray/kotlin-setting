8f784b17b1496b70130ea12ca04ca8441cbb2fa8
이용홍 <hong@mine-k.co.kr>
2020-01-13 18:24:35 +0900

    JQuery 확장 function type 설정

--- core-js/src/as.ts
+++ core-js/src/as.ts
@@ -1,5 +1,17 @@
 import * as $ from "jquery";
 
+declare global {
+    export interface JQuery {
+        asStopPropagation(): JQuery;
+
+        asPreventDefault(): JQuery;
+
+        asAnchor(): JQuery;
+
+        asComingSoon(): JQuery;
+    }
+}
+
 export let jqueryExtends = {
     asStopPropagation: function () {
         return this.each((index, element) => {
@@ -12,6 +24,22 @@ export let jqueryExtends = {
         return this.each((index, element) => {
             $(element).on('click', e => {
                 e.preventDefault();
+            });
+        });
+    },
+    asAnchor: function () {
+        return this.each((index, element) => {
+            $(element).on('click', e => {
+                e.preventDefault();
+                window.location = $(this).data('href');
+            });
+        });
+    },
+    asComingSoon: function () {
+        return this.each((index, element) => {
+            $(element).on('click', e => {
+                e.preventDefault();
+                alert('Coming Soon...')
             });
         });
     }
