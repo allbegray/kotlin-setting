b3b3a275daab3eba7975eb31df2b8c1c991d7d99
이용홍 <hong@mine-k.co.kr>
2020-01-14 19:22:52 +0900

    excel dsl 추가

---
+++ web-spring/src/main/kotlin/hong/common/excel/excel.kt
@@ -0,0 +1,211 @@
+package hong.common.excel
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook
+import org.apache.poi.ss.usermodel.*
+import org.apache.poi.xssf.streaming.SXSSFWorkbook
+import org.apache.poi.xssf.usermodel.XSSFWorkbook
+import java.io.File
+import java.io.OutputStream
+import java.net.URLEncoder
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.util.*
+import javax.servlet.http.HttpServletResponse
+
+@Suppress("SpellCheckingInspection")
+enum class WorkbookType {
+    HSSF, XSSF, SXSSF
+}
+
+class Workbook(type: WorkbookType) {
+    private val workbook: org.apache.poi.ss.usermodel.Workbook = when (type) {
+        WorkbookType.HSSF -> HSSFWorkbook()
+        WorkbookType.XSSF -> XSSFWorkbook()
+        WorkbookType.SXSSF -> SXSSFWorkbook()
+    }
+    val styles = mutableMapOf<String, CellStyle>()
+
+    fun sheet(sheetName: String, init: Sheet.() -> Unit): Sheet =
+        Sheet(this, workbook.createSheet(sheetName)).apply(init)
+
+    fun style(styleName: String, option: StyleOption) {
+        styles[styleName] = workbook.createCellStyle().apply {
+            this.setFont(workbook.createFont().apply {
+                bold = option.font.bold
+                if (option.font.height != null) {
+                    fontHeight = option.font.height
+                }
+                if (option.font.name != null) {
+                    fontName = option.font.name
+                }
+                italic = option.font.italic
+                strikeout = option.font.strikeout
+                underline = option.font.underline.byte
+            })
+            if (option.alignment != null) {
+                this.alignment = option.alignment
+            }
+            if (option.verticalAlignment != null) {
+                this.verticalAlignment = option.verticalAlignment
+            }
+            if (option.format != null) {
+                this.dataFormat = workbook.createDataFormat().getFormat(option.format)
+            }
+            this.wrapText = option.wrapText
+
+            this.borderTop = option.border.top
+            this.borderBottom = option.border.bottom
+            this.borderLeft = option.border.left
+            this.borderRight = option.border.right
+        }
+    }
+
+    fun generate(response: HttpServletResponse, fileName: String) {
+        response.contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        response.setHeader(
+            "Content-Disposition",
+            "attachement; filename=\"${URLEncoder.encode(fileName, "UTF-8")}.xlsx\";charset=\"UTF-8\""
+        )
+        this.generate(response.outputStream)
+    }
+
+    fun generate(path: String) = generate(File(path))
+
+    fun generate(path: File) = generate(path.outputStream())
+
+    fun generate(os: OutputStream) = workbook.use { os.use { workbook.write(os) } }
+}
+
+data class StyleOption(
+    val font: FontOption = FontOption(),
+    val alignment: HorizontalAlignment? = null,
+    val verticalAlignment: VerticalAlignment? = null,
+    val border: BorderOption = BorderOption(),
+    val format: String? = null,
+    val wrapText: Boolean = false
+) {
+    companion object {
+        const val CURRENCY = "#,##0"
+        const val PERCENTAGE = "0.00"
+    }
+
+    data class BorderOption(
+        val top: BorderStyle = BorderStyle.NONE,
+        val bottom: BorderStyle = BorderStyle.NONE,
+        val left: BorderStyle = BorderStyle.NONE,
+        val right: BorderStyle = BorderStyle.NONE
+    ) {
+        companion object {
+            fun of(borderStyle: BorderStyle): BorderOption {
+                return BorderOption(borderStyle, borderStyle, borderStyle, borderStyle)
+            }
+        }
+    }
+
+    data class FontOption(
+        val name: String? = null,
+        val bold: Boolean = false,
+        val height: Short? = null,
+        val italic: Boolean = false,
+        val strikeout: Boolean = false,
+        val underline: Underline = Underline.NONE
+    ) {
+        enum class Underline(val byte: Byte) {
+            NONE(Font.U_NONE), SINGLE(Font.U_SINGLE), DOUBLE(Font.U_DOUBLE)
+        }
+    }
+}
+
+class Sheet(
+    private val workbook: Workbook,
+    private val sheet: org.apache.poi.ss.usermodel.Sheet
+) {
+    private var rownum: Int = 0
+
+    fun row(height: Short = sheet.defaultRowHeight, init: Row.() -> Unit): Row {
+        return Row(workbook, this, sheet.createRow(rownum++).apply {
+            this.height = height
+        }).apply(init)
+    }
+
+    fun columnWidth(columnIndex: Int, width: Int) {
+        sheet.setColumnWidth(columnIndex, width)
+    }
+}
+
+class Row(
+    private val workbook: Workbook,
+    private val sheet: Sheet,
+    private val row: org.apache.poi.ss.usermodel.Row
+) {
+    private var column: Int = 0
+    var height: Short
+        set(value) {
+            row.height = value
+        }
+        get() = row.height
+
+    fun cell(value: String?, style: String? = null, init: (Cell.() -> Unit)? = null): Cell {
+        return cell2(value, style, init)
+    }
+
+    fun cell(
+        value: Number?,
+        defaultZero: Boolean = true,
+        style: String? = null,
+        init: (Cell.() -> Unit)? = null
+    ): Cell {
+        return cell2(value?.toDouble() ?: if (defaultZero) 0.0 else null, style)
+    }
+
+    private fun cell2(value: Any?, style: String? = null, init: (Cell.() -> Unit)? = null): Cell {
+        val cell = row.createCell(column)
+        if (value == null) {
+            cell.setCellValue("")
+        } else {
+            when (value) {
+                is Date -> cell.setCellValue(value)
+                is String -> cell.setCellValue(value)
+                is Number -> cell.setCellValue(value.toDouble())
+                is Boolean -> cell.setCellValue(value)
+                is LocalDate -> cell.setCellValue(value)
+                is LocalDateTime -> cell.setCellValue(value)
+                is RichTextString -> cell.setCellValue(value)
+                else -> cell.setCellValue(value.toString())
+            }
+        }
+        if (style != null) {
+            cell.cellStyle = workbook.styles[style]
+        }
+        return Cell(sheet, this, cell).apply {
+            if (init != null) {
+                init()
+            }
+        }
+    }
+}
+
+class Cell(
+    private val sheet: Sheet,
+    private val row: Row,
+    private val cell: org.apache.poi.ss.usermodel.Cell
+) {
+    fun columnWidth(width: Int) {
+        sheet.columnWidth(cell.columnIndex, width)
+    }
+}
+
+fun workbook(type: WorkbookType = WorkbookType.XSSF, init: Workbook.() -> Unit): Workbook = Workbook(type).apply(init)
+
+fun main() {
+    workbook {
+        style("bold", StyleOption(font = StyleOption.FontOption(bold = true)))
+        sheet("sheetName") {
+            row {
+                cell("hahaha", "bold")
+                cell("testest", "bold")
+            }
+        }
+        generate(File("asdfasdfsdaf").outputStream())
+    }
+}
